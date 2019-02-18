2bd1dc83ae835452e590fc7c0ad7b4a39193965a
zifeng44 <47290772+zifeng44@users.noreply.github.com>
2019-02-17 17:28:33 -0800

    Create height-lawnmower.js

---
+++ height-lawnmower.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(400, 200);
+  colorMode(HSB);
+}
+
+var x = 0;
+var h = 10;
+
+function draw() {
+  stroke(random(60, 70), 100, 90);
+  line(x, height-10, x+random(-10, 10), height-10-random(h));
+  noStroke();
+
+  x = x + 10;
+
+  if (x > width) {
+    x = random(10);
+    h = h + 3;
+  }
+
+  if (h > 50) {
+    fill(255);
+    rect(0, 0, width, height-15);
+    h = 10;
+  }
+
+  fill(40, 100, 60);
+  rect(0, height-10, width, 10);
+}
